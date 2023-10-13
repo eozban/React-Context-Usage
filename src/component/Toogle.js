04860493132c6ea177efa98a9d4881640816847e
Erdal Ozban <eozban@hotmail.com>
2023-10-13 17:33:17 +0300

    checkbox toogle ile değiştirildi

--- src/component/Toogle.js
+++ src/component/Toogle.js
@@ -4,8 +4,10 @@ import { PriceContext } from "../context/PriceContext";
 export default function Toogle({title}) {
     const { enabled, setEnabled } = useContext(PriceContext)
     return (
-        <div>
-            <input type="checkbox" onChange={(e)=>setEnabled(!enabled)}/> {title}
+        <div className="toogle">
+            <span className={!enabled ? "active" : ""} onClick={()=>setEnabled(false)}>Monthly</span>
+            <span className={enabled ? "active" : ""} onClick={()=>setEnabled(true)}>Yearly</span>
+            {/*<input type="checkbox" onChange={(e)=>setEnabled(!enabled)}/> {title}*/}
         </div> 
     );
 }
